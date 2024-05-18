28868a9730593439af6558d87a622845618f2992
ShubamSinghDadhwal <dadhwal.official@gmail.com>
2024-05-18 17:07:57 +0530

    crypto watchlist completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import WatchList from './Containers/Watchlist';
+
+function App() {
+  return (
+    <div className="App">
+      <WatchList />
+    </div>
+  );
+}
+
+export default App;
